2f07b27227992afc0366b4475bb7dd80d5afbb57
Jennifer Juan <jennifer.juan@spreeeed.com>
2022-03-27 20:00:30 +0800

    update assetsPrefix . -> gihub path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "."
+  assetPrefix: "https://chentingjuan.github.io/2022-jut-35"
 }
 
 module.exports = nextConfig
