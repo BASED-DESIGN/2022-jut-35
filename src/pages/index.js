c21a7c3cfaa833424189f8166081a480ad86655b
Henry Lin <onlylinihung@gmail.com>
2022-03-28 14:49:00 +0800

    upload - web font

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,17 @@ import IndexPage from '@components/doms/IndexPage'
 // About Next dynamic, React.Suspense, React.lazy discussion:
 // https://github.com/vercel/next.js/discussions/17979
 
+const favIcons = ['/icon_fav_1.png', '/icon_fav_2.png'];
+function randomItem(items){
+  return items[Math.floor(Math.random()*items.length)];
+}
+
 const Home = () => (
   <>
     <Head>
-      <title>忠泰集團 35 週年</title>
-      <meta name="description" content="忠泰集團 35 週年" />
-      {/* <link rel="icon" href="./favicon.ico" /> */}
+      <title>三五成群　破浪前行・忠泰集團 35 週年</title>
+      <meta name="description" content="我們期許以「A Better Tomorrow」的企業價值，創造一個平行於台北的平行城市、以忠泰事業系統所築構的生態體系，能獨立於城市傲然而生，同時又與原屬地共生共長，迎接每一個美好的明天。" />
+      <link rel="icon" href={randomItem(favIcons)} />
     </Head>
 
     {/* <Canvas 
